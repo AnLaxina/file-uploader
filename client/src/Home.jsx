34cc82c135225db50a630004203d231be948a38d
AnLaxina <anilovlaxina@gmail.com>
2026-02-09 12:31:55 -0800

    feat(client): add setup files

---
+++ client/src/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>File Uploader</h1>
+      </header>
+      <main>
+        <p>Welcome to the home page!</p>
+      </main>
+      <footer>
+        <p>2026 Anlaxina</p>
+      </footer>
+    </>
+  );
+}
