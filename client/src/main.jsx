7edeba590d7553a3e36e807b2f445008e9d3e817
AnLaxina <anilovlaxina@gmail.com>
2026-02-09 13:01:26 -0800

    feat(assets): add global styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./Home.jsx";
-import "./assets/main.css";
+import "./assets/css/main.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
