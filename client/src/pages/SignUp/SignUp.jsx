fdbe139c5856014f0a2daa145224584ceb8c1972
AnLaxina <anilovlaxina@gmail.com>
2026-02-12 13:48:13 -0800

    feat(signup): add layout & styling

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router";
+
 export default function SignUp() {
   return (
     <section>
-      <p>Welcome to the Sign up page!</p>
+      <h2>Sign Up</h2>
+      <form action="/api/sign-up" method="POST">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" required />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" required />
+
+        <div className="formSubmission">
+          <Link to="/" className="button">
+            Back
+          </Link>
+          <button type="submit" className="button">
+            Sign Up
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
