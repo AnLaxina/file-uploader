0057d7bfde599e0f02f9c305c89cd21be4e923c6
AnLaxina <anilovlaxina@gmail.com>
2026-02-28 17:59:02 -0800

    feat(folder): add operations for `UPDATE`, `DELETE`, & `READ`

--- server/src/controllers/folderController.js
+++ server/src/controllers/folderController.js
@@ -14,3 +14,42 @@ export async function addSingleFolder(req, res, next) {
 
   return res.send({ message: "Single folder added!" });
 }
+
+export async function getAllFoldersByOwnerId(req, res, next) {
+  const folders = await prisma.folder.findMany({
+    where: { ownerId: req.user.id },
+  });
+
+  return res.send({ folders: folders });
+}
+
+export async function deleteSingleFolder(req, res, next) {
+  if (!req.body) {
+    return res.status(400).send({ message: "Enter the folder id." });
+  }
+  const selectedFolderId = req.body.selectedFolderId;
+  await prisma.folder.delete({
+    where: {
+      id: selectedFolderId,
+    },
+  });
+
+  return res.send({ message: "Folder deleted!" });
+}
+
+export async function updateSingleFolder(req, res, next) {
+  if (!req.body) {
+    return res
+      .status(400)
+      .send({ message: "Enter the folder id and/or the updated name." });
+  }
+  const selectedFolderId = req.body.selectedFolderId;
+  const updatedFolderName = req.body.updatedFolderName;
+
+  const updatedFolder = await prisma.folder.update({
+    where: { id: selectedFolderId },
+    data: { name: updatedFolderName },
+  });
+
+  return res.send({ message: "Folder updated!", folder: updatedFolder });
+}
--- server/src/routes/folderRouter.js
+++ server/src/routes/folderRouter.js
@@ -1,10 +1,31 @@
 import { isLoggedIn } from "../controllers/loginController.js";
 import * as folderController from "../controllers/folderController.js";
-import {Router} from "express";
+import { Router } from "express";
 
 const folderRoute = Router();
 
-folderRoute.post("/api/post-single-folder", isLoggedIn, folderController.addSingleFolder);
+folderRoute.post(
+  "/api/post-single-folder",
+  isLoggedIn,
+  folderController.addSingleFolder,
+);
 
+folderRoute.get(
+  "/api/get-all-folders",
+  isLoggedIn,
+  folderController.getAllFoldersByOwnerId,
+);
+
+folderRoute.delete(
+  "/api/delete-single-folder",
+  isLoggedIn,
+  folderController.deleteSingleFolder,
+);
+
+folderRoute.patch(
+  "/api/update-single-folder",
+  isLoggedIn,
+  folderController.updateSingleFolder,
+);
 
 export default folderRoute;
