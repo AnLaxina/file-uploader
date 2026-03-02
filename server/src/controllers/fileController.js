cb6e7ae2bb0a4d2cefdcdb88871226cbbe3e5f81
AnLaxina <anilovlaxina@gmail.com>
2026-03-02 14:43:11 -0800

    refactor(file): remove unnecessary import

--- server/src/controllers/fileController.js
+++ server/src/controllers/fileController.js
@@ -6,7 +6,6 @@ import {
   DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { registerUser } from "./signupController.js";
 // S3 setup
 const s3 = new S3Client({
   region: "auto",
