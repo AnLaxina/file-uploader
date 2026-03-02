7ab60bca1c14a917435b78ea18c79a187b5cf48a
AnLaxina <anilovlaxina@gmail.com>
2026-03-01 17:23:31 -0800

    feat(file): add working feature to upload to Cloudflare

--- server/src/controllers/fileController.js
+++ server/src/controllers/fileController.js
@@ -1,5 +1,56 @@
+import { prisma } from "../lib/prisma.js";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+
+// S3 setup
+const s3 = new S3Client({
+  region: "auto",
+  endpoint: process.env.R2_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+});
+
 // * To upload things to the cloud, it would just be req.file.buffer
 
-export function addSingleFile(req, res, next) {
-  res.send({ file: req.file, body: req.body });
+export async function addSingleFile(req, res, next) {
+  if (!req.body) {
+    return res.status(400).send({ message: "Enter a file to upload." });
+  }
+  const currentDate = new Date();
+  const newFileKey = currentDate.getTime() + "-" + req.file.originalname;
+  const newFile = await prisma.file.create({
+    data: {
+      name: req.file.originalname,
+      size: req.file.size,
+      fileKey: newFileKey,
+      ownerId: req.user.id,
+      folderId: parseInt(req.body.folderId) || undefined,
+      folder: req.body.folderObject || undefined,
+    },
+  });
+
+  // Upload to Cloudflare
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET,
+      Key: newFileKey,
+      ContentType: req.file.mimetype,
+      Body: req.file.buffer,
+    }),
+  );
+
+  res.send({
+    file: req.file,
+    body: req.body,
+    message: "File uploaded!",
+    // Yeah... have to convert it to a Number as JSON doesn't support BigInts
+    newFile: { ...newFile, size: Number(req.file.size) },
+  });
 }
+
+// * Use the fileKey column from the db to get the key
--- server/src/routes/fileRouter.js
+++ server/src/routes/fileRouter.js
@@ -1,6 +1,7 @@
 import Router from "express";
 import multer from "multer";
 import * as fileController from "../controllers/fileController.js";
+import { isLoggedIn } from "../controllers/loginController.js";
 
 // * No options in multer means it'll just upload to memory
 const uploads = multer();
@@ -8,6 +9,7 @@ const uploads = multer();
 const fileRoute = Router();
 fileRoute.post(
   "/api/post-single-file",
+  isLoggedIn,
   uploads.single("file"),
   fileController.addSingleFile,
 );
