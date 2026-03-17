cfe6b03c3917813cae21b8be1ccf760f7ec4bf2a
AnLaxina <anilovlaxina@gmail.com>
2026-03-17 08:01:42 -0700

    feat: add proper styling to the `fileUpload` dialog

--- client/src/pages/ViewFolder/ViewFolder.jsx
+++ client/src/pages/ViewFolder/ViewFolder.jsx
@@ -3,9 +3,11 @@ import File from "../../components/File/File.jsx";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import axiosClient from "../../lib/axiosClient";
+import { CircleX } from "lucide-react";
 
 export default function ViewFolder() {
-  // TODO: For each file inside a folder, add a click event for viewing details such as download/delete URLs
+  // TODO: Add the ability to add/upload files
+  // But first, style the popup for this part. Right now it looks fine but you gotta add a file size limit of some kind
   const [files, setFiles] = useState([]);
   const { folderName, folderId } = useParams();
 
@@ -28,6 +30,13 @@ export default function ViewFolder() {
     ));
   }
 
+  function uploadFile(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const file = Object.fromEntries(formData).uploadedFile;
+    console.log(file);
+  }
+
   return (
     <section className={styles.viewFolderSection}>
       <h2 id={styles.header}>Viewing Folder: {folderName}</h2>
@@ -39,6 +48,42 @@ export default function ViewFolder() {
           loadFiles()
         )}
       </div>
+
+      <button
+        type="button"
+        className="button"
+        command="show-modal"
+        commandfor="uploadFileDialog"
+      >
+        Add File
+      </button>
+
+      {/* For this, I'll just add a simple dialog cause yeah I don't think I gotta make this into a component */}
+      <dialog id="uploadFileDialog" onSubmit={(e) => uploadFile(e)}>
+        <div className={styles.dialogHeader}>
+          <button
+            type="button"
+            className="emptyButton"
+            command="close"
+            commandfor="uploadFileDialog"
+          >
+            <CircleX size={30} />
+          </button>
+        </div>
+        <form action="/api/post-single-file/:folderId" method="POST">
+          <h3>Upload a File</h3>
+          <input
+            type="file"
+            name="uploadedFile"
+            id="uploadedFile"
+            required
+            autoFocus
+          />
+          <button type="submit" className="button">
+            Upload file
+          </button>
+        </form>
+      </dialog>
     </section>
   );
 }
