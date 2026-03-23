3ffa3d3c567351b322eea4ca98ef496dcf96a68c
AnLaxina <anilovlaxina@gmail.com>
2026-03-22 17:51:48 -0700

    feat(popup): add basic template for adding new folders

--- client/src/pages/ViewFiles/ViewFiles.jsx
+++ client/src/pages/ViewFiles/ViewFiles.jsx
@@ -2,11 +2,18 @@ import styles from "./viewfiles.module.css";
 import Folder from "../../components/Folder/Folder.jsx";
 import axiosClient from "../../lib/axiosClient.js";
 import { FolderContext } from "../../lib/FolderContext.js";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
+import { CircleX } from "lucide-react";
 
 export default function ViewFiles() {
   // TODO: Work on adding a form for file creation, but maybe it should be on a different page or popup modal
   const [folders, setFolders] = useState([]);
+  const [uploadMessage, setUploadMessage] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [isUploading, setIsUploading] = useState(false);
+  const formRef = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axiosClient.get("/api/get-all-folders").then((response) => {
@@ -26,6 +33,10 @@ export default function ViewFiles() {
     ));
   }
 
+  function closeDialog() {
+    formRef.current.reset();
+  }
+
   return (
     <section className={styles.viewFilesSection}>
       <FolderContext value={[folders, setFolders]}>
@@ -37,6 +48,64 @@ export default function ViewFiles() {
             loadFolders()
           )}
         </div>
+        <div className={styles.viewFilesButtons}>
+          <button type="button" className="button" onClick={() => navigate(-1)}>
+            Go Back
+          </button>
+          <button
+            type="button"
+            className="button"
+            command="show-modal"
+            commandfor="newFolderDialog"
+          >
+            Add Folder
+          </button>
+        </div>
+
+        <dialog id="newFolderDialog">
+          <div className={styles.dialogHeader}>
+            <button
+              type="reset"
+              className="emptyButton"
+              command="close"
+              commandfor="newFolderDialog"
+              onClick={closeDialog}
+            >
+              <CircleX size={30} />
+            </button>
+          </div>
+          <form
+            action="/api/post-single-file/:folderId"
+            method="POST"
+            ref={formRef}
+          >
+            <h3>New Folder</h3>
+            {uploadMessage !== "" &&
+              (isValid ? (
+                <p className={styles.validMessage}>{uploadMessage}</p>
+              ) : (
+                <p className={styles.invalidMessage}>{uploadMessage}</p>
+              ))}
+            <input
+              type="text"
+              name="folderName"
+              id="folderName"
+              placeholder="Max. 16 characters"
+              maxLength={16}
+              required
+              autoFocus
+            />
+            {isUploading ? (
+              <button type="submit" className="disabledButton" disabled>
+                Uploading...
+              </button>
+            ) : (
+              <button type="submit" className="button">
+                Create Folder
+              </button>
+            )}
+          </form>
+        </dialog>
       </FolderContext>
     </section>
   );
