2ccde6cbba5717b27737216bee7370ae83192663
AnLaxina <anilovlaxina@gmail.com>
2026-03-23 21:48:43 -0700

    feat(folder): add working addition of new folders

--- client/src/pages/ViewFiles/ViewFiles.jsx
+++ client/src/pages/ViewFiles/ViewFiles.jsx
@@ -13,6 +13,7 @@ export default function ViewFiles() {
   const [isValid, setIsValid] = useState(true);
   const [isUploading, setIsUploading] = useState(false);
   const formRef = useRef(null);
+  const dialogRef = useRef(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -35,8 +36,29 @@ export default function ViewFiles() {
 
   function closeDialog() {
     formRef.current.reset();
+    dialogRef.current.close();
   }
 
+  function addFolder(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formValues = Object.fromEntries(formData);
+    axiosClient
+      .post("/api/post-single-folder", formValues)
+      .then((response) => {
+        // Once successful, call the API endpoint again to refresh state
+        axiosClient.get("/api/get-all-folders").then((response) => {
+          const folders = response.data.folders;
+          setFolders(folders);
+        });
+
+        window.alert(response.data.message);
+
+        // Reset everything
+        closeDialog();
+      })
+      .catch((error) => console.error(error));
+  }
   return (
     <section className={styles.viewFilesSection}>
       <FolderContext value={[folders, setFolders]}>
@@ -62,15 +84,9 @@ export default function ViewFiles() {
           </button>
         </div>
 
-        <dialog id="newFolderDialog">
+        <dialog id="newFolderDialog" ref={dialogRef}>
           <div className={styles.dialogHeader}>
-            <button
-              type="reset"
-              className="emptyButton"
-              command="close"
-              commandfor="newFolderDialog"
-              onClick={closeDialog}
-            >
+            <button type="reset" className="emptyButton" onClick={closeDialog}>
               <CircleX size={30} />
             </button>
           </div>
@@ -78,8 +94,9 @@ export default function ViewFiles() {
             action="/api/post-single-file/:folderId"
             method="POST"
             ref={formRef}
+            onSubmit={(e) => addFolder(e)}
           >
-            <h3>New Folder</h3>
+            <h3>Create New Folder</h3>
             {uploadMessage !== "" &&
               (isValid ? (
                 <p className={styles.validMessage}>{uploadMessage}</p>
@@ -101,7 +118,7 @@ export default function ViewFiles() {
               </button>
             ) : (
               <button type="submit" className="button">
-                Create Folder
+                Create
               </button>
             )}
           </form>
